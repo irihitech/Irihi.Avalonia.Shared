8ce9f80120db7d55d71bc42c625f09a397d3f21a
rabbitism <dongbin@irihi.tech>
2024-02-24 23:54:38 +0800

    feat: add trybind.

---
+++ src/Irihi.Avalonia.Shared/Helpers/BindingExtension.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Data;
+
+namespace Irihi.Avalonia.Shared.Helpers;
+
+public static class BindingExtension
+{
+    public static bool TryBind(this AvaloniaObject obj, AvaloniaProperty property, IBinding? binding)
+    {
+        if (binding is null) return false;
+        obj.Bind(property, binding);
+        return true;
+    }
+}
---
+++ test/Irihi.Avalonia.Shared.UnitTest/Helpers/BindingExtensionTest.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Irihi.Avalonia.Shared.Helpers;
+
+namespace Irihi.Avalonia.Shared.UnitTest.Helpers;
+
+public class BindingExtensionTest
+{
+    [Fact]
+    public void SetBinding_Success()
+    {
+        TextBlock t = new TextBlock();
+        t.Foreground = new SolidColorBrush(Colors.Black);
+        t.Background = new SolidColorBrush(Colors.White);
+        Assert.NotEqual(t.Foreground.ToString(), t.Background.ToString());
+        bool b = t.TryBind(TextBlock.ForegroundProperty, t[!TextBlock.BackgroundProperty]);
+        Assert.True(b);
+        Assert.Equal(t.Foreground.ToString(), new SolidColorBrush(Colors.White).ToString());
+        t.Background = new SolidColorBrush(Colors.Yellow);
+        Assert.Equal(t.Foreground.ToString(), new SolidColorBrush(Colors.Yellow).ToString());
+    }
+    
+    [Fact]
+    public void SetNullBinding_Fail()
+    {
+        TextBlock t = new TextBlock();
+        t.Foreground = new SolidColorBrush(Colors.Black);
+        t.Background = new SolidColorBrush(Colors.White);
+        Assert.NotEqual(t.Foreground.ToString(), t.Background.ToString());
+        bool b = t.TryBind(TextBlock.ForegroundProperty, null);
+        Assert.False(b);
+        Assert.Equal(t.Foreground.ToString(), new SolidColorBrush(Colors.Black).ToString());
+        t.Background = new SolidColorBrush(Colors.Yellow);
+        Assert.Equal(t.Foreground.ToString(), new SolidColorBrush(Colors.Black).ToString());
+    }
+}
