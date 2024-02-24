466f5271ce86db2190ecec4f33e02e926de1f987
rabbitism <dongbin@irihi.tech>
2024-02-25 00:52:02 +0800

    feat: add observable extension.

---
+++ test/Irihi.Avalonia.Shared.UnitTest/Helpers/ObservableExtensionTest.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Irihi.Avalonia.Shared.Helpers;
+
+namespace Irihi.Avalonia.Shared.UnitTest.Helpers;
+
+public class ObservableExtensionTest
+{
+    [Fact]
+    public void Subscribe_Action_Success()
+    {
+        bool called = false;
+        var textBlock = new TextBlock();
+        IObservable<string?> observable = textBlock.GetObservable(TextBlock.TextProperty);
+        IDisposable disposable = observable.Subscribe(i => called = true);
+        textBlock.Text = "Hello, World!";
+        disposable.Dispose();
+        Assert.True(called);
+    }
+}
