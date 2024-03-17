4f3dd98c68c3f7f337951f2373db4b7907acbc26
rabbitism <dongbin@irihi.tech>
2024-03-17 23:57:09 +0800

    feat: add more names.

--- src/Irihi.Avalonia.Shared/AssemblyInfo.cs
+++ src/Irihi.Avalonia.Shared/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Metadata;
 
 [assembly:XmlnsPrefix("https://irihi.tech/shared", "iri")]
+[assembly:XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Common")]
 [assembly:XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Contracts")]
 [assembly:XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Property")]
 [assembly:XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.MarkupExtensions")]
