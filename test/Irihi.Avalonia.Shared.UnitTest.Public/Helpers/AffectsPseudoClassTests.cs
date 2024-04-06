8154424c8baf24928e20d6b75802d0943df21741
rabbitism <dongbin@irihi.tech>
2024-04-06 22:22:14 +0800

    test: remove warning in test.

--- test/Irihi.Avalonia.Shared.UnitTest.Public/Helpers/AffectsPseudoClassTests.cs
+++ test/Irihi.Avalonia.Shared.UnitTest.Public/Helpers/AffectsPseudoClassTests.cs
@@ -7,21 +7,40 @@ namespace Irihi.Avalonia.Shared.UnitTest.Helpers;
 
 public class AffectsPseudoClassTests
 {
+    private class Sample : Control
+    {
+        public static readonly StyledProperty<bool> TestProperty = AvaloniaProperty.Register<Sample, bool>(
+            nameof(Test));
+
+        public bool Test
+        {
+            get => GetValue(TestProperty);
+            set => SetValue(TestProperty, value);
+        }
+
+        public static readonly RoutedEvent<RoutedEventArgs> TestChangedEvent =
+            RoutedEvent.Register<Sample, RoutedEventArgs>(nameof(TestChanged), RoutingStrategies.Bubble);
+        
+        public event EventHandler<RoutedEventArgs> TestChanged
+        {
+            add => AddHandler(TestChangedEvent, value);
+            remove => RemoveHandler(TestChangedEvent, value);
+        }
+        
+        static Sample()
+        {
+            TestProperty.AffectsPseudoClass<Sample, RoutedEventArgs>(":test", TestChangedEvent); 
+        }
+    }
+    
     [Fact]
     public void TestAffectsPseudoClass()
     {
-        // Arrange
-        var button = new Button();
-        var property = AvaloniaProperty.Register<Button, bool>("TestProperty");
+        Sample sample = new();
         bool flag = false;
-        button.Click += (sender, e) => flag = true;
-
-        // Act
-        property.AffectsPseudoClass<Button, RoutedEventArgs>(pseudoClass: "test", Button.ClickEvent);
-        property.SetValue(true, button);
-        
-        // Assert
-        Assert.Contains("test", button.Classes);
+        sample.TestChanged += (_, _) => flag = true;
+        sample.Test = true;
+        Assert.Contains(sample.Classes, x => x == ":test");
         Assert.True(flag);
     }
 }
