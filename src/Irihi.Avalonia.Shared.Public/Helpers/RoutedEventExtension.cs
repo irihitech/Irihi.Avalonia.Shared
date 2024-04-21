4f21cb1e466672c9f7f5c452327434418a539bf9
rabbitism <dongbin@irihi.tech>
2024-04-21 11:55:55 +0800

    feat: add more methods.

--- src/Irihi.Avalonia.Shared.Public/Helpers/RoutedEventExtension.cs
+++ src/Irihi.Avalonia.Shared.Public/Helpers/RoutedEventExtension.cs
@@ -1,4 +1,3 @@
-using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Irihi.Avalonia.Shared.Reactive;
 
@@ -6,7 +5,10 @@ namespace Irihi.Avalonia.Shared.Helpers;
 
 public static class RoutedEventExtension
 {
-    public static void AddHandler<TArgs>(this RoutedEvent<TArgs> routedEvent, EventHandler<TArgs> handler, params Interactive?[] controls)
+    public static void AddHandler<TArgs>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        params Interactive?[] controls)
         where TArgs : RoutedEventArgs
     {
         foreach (var t in controls)
@@ -15,7 +17,11 @@ public static class RoutedEventExtension
         }
     }
     
-    public static void AddHandler<TArgs, TControl>(this RoutedEvent<TArgs> routedEvent, EventHandler<TArgs> handler, params TControl?[] controls) where TControl: Interactive
+    public static void AddHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        params TControl?[] controls) 
+        where TControl: Interactive
         where TArgs : RoutedEventArgs
     {
         foreach (var t in controls)
@@ -24,7 +30,8 @@ public static class RoutedEventExtension
         }
     }
     
-    public static void AddHandler<TArgs>(this RoutedEvent<TArgs> routedEvent, 
+    public static void AddHandler<TArgs>(
+        this RoutedEvent<TArgs> routedEvent, 
         EventHandler<TArgs> handler, 
         RoutingStrategies strategies = RoutingStrategies.Bubble | RoutingStrategies.Direct, 
         bool handledEventsToo = false, 
@@ -37,7 +44,8 @@ public static class RoutedEventExtension
         }
     }
     
-    public static void AddHandler<TArgs, TControl>(this RoutedEvent<TArgs> routedEvent, 
+    public static void AddHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
         EventHandler<TArgs> handler, 
         RoutingStrategies strategies = RoutingStrategies.Bubble | RoutingStrategies.Direct, 
         bool handledEventsToo = false, 
@@ -51,7 +59,25 @@ public static class RoutedEventExtension
         }
     }
     
-    public static void RemoveHandler<TArgs>(this RoutedEvent<TArgs> routedEvent, EventHandler<TArgs> handler, params Interactive?[] controls)
+    public static void AddHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        IEnumerable<TControl> controls, 
+        RoutingStrategies strategies = RoutingStrategies.Bubble | RoutingStrategies.Direct, 
+        bool handledEventsToo = false)
+        where TArgs : RoutedEventArgs
+        where TControl: Interactive
+    {
+        foreach (var t in controls)
+        {
+            t?.AddHandler(routedEvent, handler, strategies, handledEventsToo);
+        }
+    }
+    
+    public static void RemoveHandler<TArgs>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        params Interactive?[] controls)
         where TArgs : RoutedEventArgs
     {
         foreach (var t in controls)
@@ -60,7 +86,10 @@ public static class RoutedEventExtension
         }
     }
     
-    public static void RemoveHandler<TArgs, TControl>(this RoutedEvent<TArgs> routedEvent, EventHandler<TArgs> handler, params TControl?[] controls)
+    public static void RemoveHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        params TControl?[] controls)
         where TArgs : RoutedEventArgs
         where TControl: Interactive
     {
@@ -70,7 +99,23 @@ public static class RoutedEventExtension
         }
     }
     
-    public static IDisposable AddDisposableHandler<TArgs>(this RoutedEvent<TArgs> routedEvent, EventHandler<TArgs> handler, params Interactive?[] controls)
+    public static void RemoveHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        IEnumerable<TControl> controls)
+        where TArgs : RoutedEventArgs
+        where TControl: Interactive
+    {
+        foreach (var t in controls)
+        {
+            t?.RemoveHandler(routedEvent, handler);
+        }
+    }
+    
+    public static IDisposable AddDisposableHandler<TArgs>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        params Interactive?[] controls)
         where TArgs : RoutedEventArgs
     {
         List<IDisposable?> list = new List<IDisposable?>(controls.Length);
@@ -86,7 +131,10 @@ public static class RoutedEventExtension
         return result;
     }
     
-    public static IDisposable AddDisposableHandler<TArgs, TControl>(this RoutedEvent<TArgs> routedEvent, EventHandler<TArgs> handler, params TControl?[] controls)
+    public static IDisposable AddDisposableHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        params TControl?[] controls)
         where TArgs : RoutedEventArgs
         where TControl: Interactive
     {
@@ -103,7 +151,8 @@ public static class RoutedEventExtension
         return result;
     }
     
-    public static IDisposable AddDisposableHandler<TArgs>(this RoutedEvent<TArgs> routedEvent, 
+    public static IDisposable AddDisposableHandler<TArgs>(
+        this RoutedEvent<TArgs> routedEvent, 
         EventHandler<TArgs> handler, 
         RoutingStrategies strategies = RoutingStrategies.Bubble | RoutingStrategies.Direct, 
         bool handledEventsToo = false, 
@@ -123,7 +172,8 @@ public static class RoutedEventExtension
         return result;
     }
     
-    public static IDisposable AddDisposableHandler<TArgs, TControl>(this RoutedEvent<TArgs> routedEvent, 
+    public static IDisposable AddDisposableHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
         EventHandler<TArgs> handler, 
         RoutingStrategies strategies = RoutingStrategies.Bubble | RoutingStrategies.Direct, 
         bool handledEventsToo = false, 
@@ -132,6 +182,29 @@ public static class RoutedEventExtension
         where TControl: Interactive
     {
         List<IDisposable?> list = new List<IDisposable?>(controls.Length);
+        foreach (var t in controls)
+        {
+            var disposable = t?.AddDisposableHandler(routedEvent, handler, strategies, handledEventsToo);
+            if (disposable != null)
+            {
+                list.Add(disposable);
+            }
+        }
+        var result = new ReadonlyDisposableCollection(list);
+        return result;
+    }
+    
+    public static IDisposable AddDisposableHandler<TArgs, TControl>(
+        this RoutedEvent<TArgs> routedEvent, 
+        EventHandler<TArgs> handler, 
+        IEnumerable<TControl> controls, 
+        RoutingStrategies strategies = RoutingStrategies.Bubble | RoutingStrategies.Direct, 
+        bool handledEventsToo = false)
+        where TArgs : RoutedEventArgs
+        where TControl: Interactive
+    {
+        // list is not initialized with controls.Count() to avoid multiple enumeration
+        var list = new List<IDisposable?>();
         foreach (var t in controls)
         {
             var disposable = t?.AddDisposableHandler(routedEvent, handler, strategies, handledEventsToo);
