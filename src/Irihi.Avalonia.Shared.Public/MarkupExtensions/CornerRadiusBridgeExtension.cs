03974376d432693dcad8be530339ba3de6f2960a
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-04-08 15:03:28 +0800

    feat: add new MarkupExtensions —— ThicknessBridge & CornerRadiusBridge.

---
+++ src/Irihi.Avalonia.Shared.Public/MarkupExtensions/CornerRadiusBridgeExtension.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia;
+
+namespace Irihi.Avalonia.Shared.MarkupExtensions;
+
+public class CornerRadiusBridgeExtension : IMarkupExtension<CornerRadius>
+{
+    public double TopLeft { get; set; }
+    public double TopRight { get; set; }
+    public double BottomRight { get; set; }
+    public double BottomLeft { get; set; }
+
+    public CornerRadiusBridgeExtension()
+    {
+    }
+
+    public CornerRadiusBridgeExtension(double uniformRadius)
+    {
+        TopLeft = TopRight = BottomLeft = BottomRight = uniformRadius;
+    }
+
+    public CornerRadiusBridgeExtension(double top, double bottom)
+    {
+        TopLeft = TopRight = top;
+        BottomLeft = BottomRight = bottom;
+    }
+
+    public CornerRadiusBridgeExtension(double topLeft, double topRight, double bottomRight, double bottomLeft)
+    {
+        TopLeft = topLeft;
+        TopRight = topRight;
+        BottomRight = bottomRight;
+        BottomLeft = bottomLeft;
+    }
+
+    public CornerRadius ProvideValue(IServiceProvider serviceProvider)
+    {
+        return new CornerRadius(TopLeft, TopRight, BottomRight, BottomLeft);
+    }
+}
---
+++ src/Irihi.Avalonia.Shared.Public/MarkupExtensions/ThicknessBridgeExtension.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia;
+
+namespace Irihi.Avalonia.Shared.MarkupExtensions;
+
+public class ThicknessBridgeExtension : IMarkupExtension<Thickness>
+{
+    public double Left { get; set; }
+    public double Top { get; set; }
+    public double Right { get; set; }
+    public double Bottom { get; set; }
+
+    public ThicknessBridgeExtension()
+    {
+    }
+
+    public ThicknessBridgeExtension(double uniformLength)
+    {
+        Left = Right = Top = Bottom = uniformLength;
+    }
+
+    public ThicknessBridgeExtension(double horizontal, double vertical)
+    {
+        Left = Right = horizontal;
+        Top = Bottom = vertical;
+    }
+
+    public ThicknessBridgeExtension(double left, double top, double right, double bottom)
+    {
+        Left = left;
+        Top = top;
+        Right = right;
+        Bottom = bottom;
+    }
+
+    public Thickness ProvideValue(IServiceProvider serviceProvider)
+    {
+        return new Thickness(Left, Top, Right, Bottom);
+    }
+}
