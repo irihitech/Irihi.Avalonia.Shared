f90bffdbe90f37e91f554994a58d61aee2328a54
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-04-09 21:41:06 +0800

    feat: add new Converters —— ThicknessConverter & CornerRadiusConverter.

---
+++ src/Irihi.Avalonia.Shared.Public/Converters/ThicknessConverter.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Irihi.Avalonia.Shared.MarkupExtensions;
+
+namespace Irihi.Avalonia.Shared.Converters;
+
+public class ThicknessConverter : IMarkupExtension<IValueConverter>, IValueConverter
+{
+    private readonly ThicknessPosition _position = ThicknessPosition.All;
+
+    public ThicknessConverter()
+    {
+    }
+
+    public ThicknessConverter(ThicknessPosition position)
+    {
+        _position = position;
+    }
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Thickness t)
+        {
+            var left = _position.HasFlag(ThicknessPosition.Left) ? t.Left : 0d;
+            var top = _position.HasFlag(ThicknessPosition.Top) ? t.Top : 0d;
+            var right = _position.HasFlag(ThicknessPosition.Right) ? t.Right : 0d;
+            var bottom = _position.HasFlag(ThicknessPosition.Bottom) ? t.Bottom : 0d;
+            return new Thickness(left, top, right, bottom);
+        }
+
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IValueConverter ProvideValue(IServiceProvider serviceProvider)
+    {
+        return this;
+    }
+}
+
+[Flags]
+public enum ThicknessPosition
+{
+    None = 0,
+    Left = 1,
+    Top = 2,
+    Right = 4,
+    Bottom = 8,
+    TopLeft = Top | Left,
+    TopRight = Top | Right,
+    BottomRight = Bottom | Right,
+    BottomLeft = Bottom | Left,
+    Horizontal = Left | Right,
+    Vertical = Top | Bottom,
+    All = Left | Top | Right | Bottom,
+}
--- src/Irihi.Avalonia.Shared/AssemblyInfo.cs
+++ src/Irihi.Avalonia.Shared/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using Avalonia.Metadata;
 [assembly: XmlnsPrefix("https://irihi.tech/shared", "iri")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Common")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Contracts")]
+[assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Converters")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Property")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.MarkupExtensions")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Shapes")]
