97fd1a559097db1b79f1f36187263c74f781f5f3
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-04-24 23:31:30 +0800

    misc: add XmlnsDefinition for Helpers namespace.

--- src/Irihi.Avalonia.Shared/AssemblyInfo.cs
+++ src/Irihi.Avalonia.Shared/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using Avalonia.Metadata;
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Common")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Contracts")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Converters")]
+[assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Helpers")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Property")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.MarkupExtensions")]
 [assembly: XmlnsDefinition("https://irihi.tech/shared", "Irihi.Avalonia.Shared.Shapes")]
