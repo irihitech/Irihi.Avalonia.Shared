28608e9f21d1646057bac59a4232b235d239b891
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-02-06 19:17:27 +0800

    feat: update Avalonia version to 12.0.999-cibuild0061987-alpha and add NuGet configuration.

--- src/Irihi.Avalonia.Shared.Public/Helpers/BindingExtension.cs
+++ src/Irihi.Avalonia.Shared.Public/Helpers/BindingExtension.cs
@@ -6,7 +6,7 @@ namespace Irihi.Avalonia.Shared.Helpers;
 
 public static class BindingExtension
 {
-    public static ResultDisposable TryBind(this AvaloniaObject obj, AvaloniaProperty property, IBinding? binding)
+    public static ResultDisposable TryBind(this AvaloniaObject obj, AvaloniaProperty property, BindingBase? binding)
     {
         if (binding is null) return new ResultDisposable(new EmptyDisposable(), false);
         return new ResultDisposable(obj.Bind(property, binding), true);
