309b847daeea52dae0c19055fb9e3f9ce31efb83
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-03-06 14:34:35 +0800

    chore: upgrade actions and package versions to 12.0.0-preview2.

--- sample/Sample/App.axaml.cs
+++ sample/Sample/App.axaml.cs
@@ -9,6 +9,9 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+#if DEBUG
+        this.AttachDeveloperTools();
+#endif
     }
 
     public override void OnFrameworkInitializationCompleted()
